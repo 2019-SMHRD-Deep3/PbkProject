3d0549dfeb59ae374b361544efc6ef86f160d777
USER <USER@DESKTOP-8MIN3BYK>
2025-10-04 14:39:40 +0900

    testController 추가

---
+++ SpringGit/src/main/java/kr/controller/controller/MemberController.java
@@ -0,0 +1,14 @@
+package kr.controller.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MemberController {
+
+	@RequestMapping("/test")
+	public @ResponseBody String test(){
+		return "hello Git";
+	}
+}
